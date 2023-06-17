0c9045a4703007b483a3b153dcfa67ce7e49d38b
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-06-18 02:02:09 +0800

    [ui][Adrian] fucked with recoil and nextjs13 routing

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { authModalState } from "@/atoms/authModalAtom";
 import Image from "next/image";
 import Link from "next/link";
+import { useSetRecoilState } from "recoil";
 
 interface NavbarProps {}
 
 export function Navbar({}: NavbarProps) {
+	const setAuthModalState = useSetRecoilState(authModalState);
+
+	const handleClick = () => {
+		setAuthModalState((prev) => ({ ...prev, isOpen: true }));
+	};
+
 	return (
 		<div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
 			<Link href="/" className="flex items-center justify-center h-20">
@@ -17,7 +27,10 @@ export function Navbar({}: NavbarProps) {
 			</Link>
 
 			<div className="flex items-center">
-				<button className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md font-medium border-2 border-brand-orange hover:text-brand-orange hover:bg-white hover:border-2 transition duration-300 ease-in-out">
+				<button
+					className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md font-medium border-2 border-brand-orange hover:text-brand-orange hover:bg-white hover:border-2 transition duration-300 ease-in-out"
+					onClick={handleClick}
+				>
 					Sign In
 				</button>
 			</div>
