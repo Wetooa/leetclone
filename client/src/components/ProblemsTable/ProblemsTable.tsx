6c2df0e35b433f239bf4c4431f97ab73b40fb151
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-06-21 03:49:50 +0800

    [ui][Adrian] stoping ansidaisbda

---
+++ client/src/components/ProblemsTable/ProblemsTable.tsx
@@ -0,0 +1,52 @@
+import { problems } from "@/mockProblems/problems";
+import Link from "next/link";
+import { BsCheckCircle } from "react-icons/bs";
+import { AiFillYoutube } from "react-icons/ai";
+
+interface ProblemsTableProps {}
+
+export default function ProblemsTable({}: ProblemsTableProps) {
+	return (
+		<tbody className="text-white">
+			{problems.map((doc, index) => {
+				const difficultyColor =
+					doc.difficulty === "Easy"
+						? "text-dark-green-s"
+						: doc.difficulty === "Medium"
+						? "text-dark-yellow"
+						: "text-dark-pink";
+
+				return (
+					<tr
+						className={`${index % 2 == 0 ? "bg-dark-layer-1" : ""}`}
+						key={doc.id}
+					>
+						<th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
+							<BsCheckCircle fontSize={"18"} width="18" />
+						</th>
+						<td className="px-6 py-4">
+							<Link
+								className="hover:text-blue-600 cursor-pointer"
+								href={`/problems/${doc.id}`}
+							>
+								{doc.title}
+							</Link>
+						</td>
+						<td className={`px-6 py-4 ${difficultyColor}`}>{doc.difficulty}</td>
+						<td className="px-6 py-4">{doc.category}</td>
+						<td className="px-6 py-4">
+							{doc.videoId ? (
+								<AiFillYoutube
+									className="cursor-pointer hover:text-red-600"
+									fontSize={"18"}
+								/>
+							) : (
+								<p className="text-gray-400">Coming soon</p>
+							)}
+						</td>
+					</tr>
+				);
+			})}
+		</tbody>
+	);
+}
