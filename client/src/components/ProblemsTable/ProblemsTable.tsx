8483db1c6593ce95177bc60f9e0bb98035073efc
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-07-10 05:21:38 +0800

    [ui][Adrian] added solved problems ui

--- client/src/components/ProblemsTable/ProblemsTable.tsx
+++ client/src/components/ProblemsTable/ProblemsTable.tsx
@@ -10,10 +10,12 @@ import { DBProblem } from "@/utils/types/problem";
 
 interface ProblemsTableProps {
 	setLoadingProblems: Dispatch<SetStateAction<boolean>>;
+	solvedProblems: string[];
 }
 
 export default function ProblemsTable({
 	setLoadingProblems,
+	solvedProblems,
 }: ProblemsTableProps) {
 	const problems: Array<DBProblem> = useGetProblems(setLoadingProblems);
 
@@ -21,6 +23,12 @@ export default function ProblemsTable({
 		isOpen: false,
 		videoId: "",
 	});
+
+	const difficultyColor: { [key: string]: string } = {
+		Easy: "text-dark-green-s",
+		Medium: "text-dark-yellow",
+		Hard: "text-dark-pink",
+	};
 
 	const closeModal = () => {
 		setYoutubePlayer({ isOpen: false, videoId: "" });
@@ -38,32 +46,28 @@ export default function ProblemsTable({
 		<>
 			<tbody className="text-white">
 				{problems.map((doc, index) => {
-					const difficultyColor =
-						doc.difficulty === "Easy"
-							? "text-dark-green-s"
-							: doc.difficulty === "Medium"
-							? "text-dark-yellow"
-							: "text-dark-pink";
-
 					return (
 						<tr
 							className={`${index % 2 == 0 ? "bg-dark-layer-1" : ""}`}
 							key={doc.id}
 						>
 							<th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
-								<BsCheckCircle fontSize={"18"} width="18" />
+								{solvedProblems.includes(doc.id) && (
+									<BsCheckCircle fontSize={"18"} width="18" />
+								)}
 							</th>
+
 							<td className="px-6 py-4">
 								<Link
 									className="hover:text-blue-600 cursor-pointer"
-									href={doc.link || `/problems/${doc.id}`}
+									href={doc.link ?? `/problems/${doc.id}`}
 								>
 									{doc.order}
 									{". "}
 									{doc.title}
 								</Link>
 							</td>
-							<td className={`px-6 py-4 ${difficultyColor}`}>
+							<td className={`px-6 py-4 ${difficultyColor[doc.difficulty]}`}>
 								{doc.difficulty}
 							</td>
 							<td className="px-6 py-4">{doc.category}</td>
